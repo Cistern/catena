c179f7fd2553c6f5d059ffc46b38db2928211d3d
Preetam Jinka <pj@preet.am>
2015-03-17 12:18:24 -0400

    Add comments to compact.go

--- compact.go
+++ compact.go
@@ -10,8 +10,11 @@ import (
 	"github.com/PreetamJinka/catena/partition/memory"
 )
 
+// compact drops old partitions and compacts older memory to
+// read-only disk partitions.
 func (db *DB) compact() {
 
+	// Look for partitions to drop
 	i := db.partitionList.NewIterator()
 	seen := 0
 	lastMin := int64(0)
@@ -27,19 +30,18 @@ func (db *DB) compact() {
 			continue
 		}
 
-		db.minTimestamp = lastMin
-
 		atomic.SwapInt64(&db.minTimestamp, lastMin)
 
+		// Remove it from the list
 		db.partitionList.Remove(p)
 
+		// Make sure we're the only ones accessing the partition
 		p.ExclusiveHold()
-
 		p.Destroy()
 		p.ExclusiveRelease()
-
 	}
 
+	// Find partitions to compact
 	toCompact := []partition.Partition{}
 
 	seen = 0
@@ -47,15 +49,14 @@ func (db *DB) compact() {
 	for i.Next() {
 		seen++
 		if seen <= 2 {
+			// Skip the latest two in-memory partitions
 			continue
 		}
 
 		p, _ := i.Value()
 
 		if !p.ReadOnly() {
-
 			p.ExclusiveHold()
-
 			p.SetReadOnly()
 			p.ExclusiveRelease()
 
@@ -64,7 +65,10 @@ func (db *DB) compact() {
 	}
 
 	for _, p := range toCompact {
+		// p is read-only, so no need to lock.
 		memPart := p.(*memory.MemoryPartition)
+		
+		// Create the disk partition file
 		filename := strings.TrimSuffix(memPart.Filename(), ".wal") + ".part"
 		f, err := os.Create(filename)
 		if err != nil {
@@ -72,12 +76,14 @@ func (db *DB) compact() {
 			return
 		}
 
-		memPart.Compact(f)
+		// Compact
+		err = memPart.Compact(f)
 		if err != nil {
 			// ???
 			return
 		}
 
+		// Close and reopen.
 		f.Sync()
 		f.Close()
 
@@ -87,6 +93,7 @@ func (db *DB) compact() {
 			return
 		}
 
+		// Swap the memory partition with the disk partition.
 		db.partitionList.Swap(memPart, diskPart)
 
 		memPart.Destroy()
